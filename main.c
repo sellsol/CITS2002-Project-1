d76727de17cb6d22dca5804d0c86f3b44f82dd2e
Semuca <pokelock@icloud.com>
2022-08-26 10:59:08 +0800

    Starting a function; showing heidi how git works

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int howManyTimesaMonth(char monthProvided, char minutes, char hours, char days, char months, char weekdays) {
+	int times = 1;
+	if (minutes == '*') {
+		times *= 60;
+	}
+	if (hours == '*') {
+		times *= 24;
+	}
+	if (days == '*') {
+		times *= :
+	}
+
+}
+
+int main(int argcount, char *argvalue[]) {
+	
+
+	exit(EXIT_SUCCESS);
+}
